722413f6b82aa24bb658614a22c5b95bcabd319c
Camilo Cunha de Azevedo <camilotk@gmail.com>
2022-04-24 23:40:28 -0300

    Refactor the XHR

--- src/app/pages/answer.cljs
+++ src/app/pages/answer.cljs
@@ -2,21 +2,16 @@
 
 (defn get-advice []
   (-> (js/fetch "https://api.adviceslip.com/advice")
-      (.then (fn [r]
-               (when-not (.-ok r)
-                 (throw (js/Error. "Could not fetch /data")))
-               (.json r)))
-      (.then (fn [r] (do
-                       (js/console.log "%cJSON" "color: white; font-size: 2em;background-color:orange;padding 10px" r)
-                       (-> js/document
-                           (.querySelector "h1")
-                           (.-textContent)
-                           (set! (aget r "slip" "advice"))))))
-      (.catch (fn [e]
-                (prn [:error e])))))
+      (.then #(do (when-not (.-ok %)
+                    (throw (js/Error. "Could not fetch the data")))
+                  (.json %)))
+      (.then #((-> js/document
+                   (.querySelector "h1")
+                   (.-textContent)
+                   (set! (aget % "slip" "advice")))))
+      (.catch #(prn [:error %]))))
 
 (defn answer []
-  ; (.addEventListener js/window "beforeunload" (fn [e] (js/console.log "a")))
   (get-advice)
   [:<>
    [:div.layout
@@ -25,3 +20,6 @@
      [:button {:class "ask-for-advice"} [:a.link-advice {:on-click get-advice} "Another"]]]
     [:img {:src "https://raw.githubusercontent.com/geovannabrgs/wisegoat/master/assets/bodezinho.png"}]]
    [:div.container-home  [:a.home-link {:href "/"} "Home"]]])
+
+; (js/console.log "%cJSON" "color: white; font-size: 2em;background-color:orange;padding 10px" r)
+; (.addEventListener js/window "beforeunload" (fn [e] (js/console.log "a")))
